3dd8c1c99545012315728f8e45c2e54b0638893d
Ajith <ajithmuge6@gmail.com>
2026-05-02 17:41:24 +0530

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import Login from './components/Login';
 import Nav from './components/Nav';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
+import Ballpit from './components/Ballpit';
 
 
 function App () {
@@ -28,6 +29,13 @@ function App () {
   return (
 
     <HashRouter>
+      <Ballpit
+        count={200}
+        gravity={0.7}
+        friction={0.9975}
+        wallBounce={0.95}
+        followCursor={true}
+      />
       <Nav />
       <Routes>
         <Route path='/' element={<Home />} />
